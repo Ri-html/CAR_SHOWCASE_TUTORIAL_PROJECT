bdc00511db3870a32f4da2f2bb3a3f991ee12930
Harry He <harry3145678@gmail.com>
2024-07-29 18:05:11 -0600

    changed custom button component to display variable title and include button type

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,16 +5,16 @@ import Image from 'next/image';
 import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({title,containerStyles,
-    handleClick}: CustomButtonProps) => {
+    handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={"button"}
+    type={btnType || "button"}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >
         <span className="flex-1">
-            title
+            {title}
         </span>
     </button>
   )
